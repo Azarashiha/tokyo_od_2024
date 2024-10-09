1214899c7bcf2cda5572af4880e9f280a8b4d96c
Azarashiha <azarashiha.g@gmail.com>
2024-10-09 21:09:06 +0900

    pbf test

--- tokyo2024/lib/pages/map_page.dart
+++ tokyo2024/lib/pages/map_page.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 class MapPage extends StatefulWidget {
-  const MapPage();
-
   @override
-  State createState() => FullMapState();
+  _MapPageState createState() => _MapPageState();
 }
 
-class FullMapState extends State<MapPage> {
+class _MapPageState extends State<MapPage> {
   MapboxMap? mapboxMap;
 
   void _onMapCreated(MapboxMap mapboxMap) {
@@ -16,17 +14,42 @@ class FullMapState extends State<MapPage> {
     mapboxMap.loadStyleURI('mapbox://styles/mapbox/standard');
   }
 
+
+  void _onStyleLoadedCallback(StyleLoadedEventData data) async {
+    // Add the vector tile source
+    await mapboxMap?.style.addSource(VectorSource(
+      id: 'tokyo-od-source',
+      tiles: [
+        'https://azarashiha.github.io/tokyo_od_2024/pbf_tiles/{z}/{x}/{y}.pbf'
+      ],
+      minzoom: 0,
+      maxzoom: 14,
+    ));
+
+    // Add a layer to display the vector tiles
+    await mapboxMap?.style.addLayer(
+      LineLayer(
+        id: 'tokyo-od-layer',
+        sourceId: 'tokyo-od-source',
+        sourceLayer: 'your-source-layer-name', // Replace with the actual layer name
+        lineColor: Colors.red.value,
+        lineWidth: 2.0,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    // dotenv.get("MAPBOX_API") の呼び出しを削除
-    // print(dotenv.get("MAPBOX_API"));
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('Mapbox Flutter Demo'),
-      // ),
       body: MapWidget(
-        key: const ValueKey("mapWidget"),
+        key: ValueKey("mapWidget"),
+        styleUri: MapboxStyles.LIGHT,
+        cameraOptions: CameraOptions(
+          center: Point(coordinates: Position(139.6917, 35.6895)), // Tokyo coordinates
+          zoom: 12.0,
+        ),
         onMapCreated: _onMapCreated,
+        onStyleLoadedListener: _onStyleLoadedCallback,
       ),
     );
   }
